0fb7dfcbe0dedc0ad88f654239ddd3aba5cf38fa
Sven Nolting | Hochwarth IT GmbH <nolting@hochwarth-it.de>
2026-09-17 14:59:15 +0200

    fix: correct catalog migration namespaces

--- migrations/Version20260917150000.php
+++ migrations/Version20260917150000.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use DoctrineDBALSchemaSchema;
-use DoctrineMigrationsAbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 final class Version20260917150000 extends AbstractMigration
 {
